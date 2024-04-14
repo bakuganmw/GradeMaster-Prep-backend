3d75fe097a6c6c8230101408d607bb8201400373
Trung Anh Nguyen Nang <101046115+bakuganmw@users.noreply.github.com>
2024-04-14 15:07:44 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const secret = '92fa9dfaf9dc42d5286b30d9b6896cfdacd862477b62b262bf1a000761a1b896';
+const secret = 'changeToLongValueToDecypher';
 const port = 4000;
 const mongoURL = 'url for mongo'
 export {secret,port,mongoURL}
